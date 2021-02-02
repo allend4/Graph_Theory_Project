b9bd2a76aa5f121dd94cb4a405c7630f31f43e74
allend4 <g00375372@gmit.ie>
2021-02-02 22:57:49 +0000

    Looked at basics.

---
+++ basics.py
@@ -0,0 +1,17 @@
+# David Allen
+# Python Basics
+# 2021-02-02
+
+print("Hello world!")
+
+a = 1
+b = 1.0
+s = "Hello world from a string"
+t = 'Hello from a different string'
+
+print(a, b, s, t) 
+
+print(type(a)) # prints type, eg int, string
+print(s[0]) # prints letter in s - placement 0 is H
+print(s[0:2]) # string slice - shows characters between 0-2 (but not 2)
+print(s[3:10:2]) # start at index 3, jump up 2 everytime until limit 10 - character 3, 5, 7 ,9

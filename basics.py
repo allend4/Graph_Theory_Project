a9baadfbebe98d2cc635e45db760d37326f7812c
allend4 <g00375372@gmit.ie>
2021-02-03 10:19:05 +0000

    Basics updated - functions, ifs, and whiles.

--- basics.py
+++ basics.py
@@ -45,3 +45,28 @@ r = [1, 2, 3, 4]
 print(r)
 s = [i*i for i in r]
 print(s)
+
+# function defined by def
+def factorial(n):
+    """Number to calculate factorial of.""" # doc string
+    # deals with negative input
+    if n < 1:
+        m = -n
+    else:
+        m = n
+    # the running total - eventually the factorial
+    total = 1
+    # loop to do the multiplications
+    while m > 1: # while loop
+        total = total * m
+        m = m - 1
+    # return the answer
+    if n < 1:
+        return -total
+    else:
+        return total
+
+# test function
+n = 20    
+# calculate the factorial of n 
+print(f"The factorial of {n} is {factorial(n)}.") # f string below

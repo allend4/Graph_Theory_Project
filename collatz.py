72d7a18f720570c98cc2d5d3f32fd6f2fbe36ee5
allend4 <g00375372@gmit.ie>
2021-02-10 11:24:50 +0000

    collatz completed

--- collatz.py
+++ collatz.py
@@ -5,7 +5,7 @@
 def f(n):
     # If n is even
     if n % 2 == 0:
-        return n / 2
+        return n // 2 # // interger division, otherwise / allocates float
     #If n is odd
     elif n % 2 == 1:
         return (3 * n) + 1
@@ -13,11 +13,18 @@ def f(n):
         return None
 
 def collatz(n):
+    so_far = []
     # loop unitl n is 1
     while n != 1:
+
+        if n in so_far:
+            return False
+        so_far.append(n)
         n = f(n)
-        return True
 
-print(collatz(10))
-print(collatz())
+    so_far.append(n)    
+    return so_far
 
+print(collatz(10)) 
+print(collatz(27)) 
+# collatz conjecture true for 10, 27

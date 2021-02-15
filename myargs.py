5f9d083507fcfec32419e2e1bc94e84a26631a47
allend4 <g00375372@gmit.ie>
2021-02-15 23:54:31 +0000

    argparse

---
+++ myargs.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# linux shebang
+import argparse
+
+# instance
+parser = argparse.ArgumentParser(description='Process some integers.')
+# arguements
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
